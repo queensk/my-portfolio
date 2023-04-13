e61206d20a4be27e820da0578cc5e8bd9c719b7f
queens kisivuli <queenskisivuli@gmail.com>
2023-04-13 07:11:14 +0300

    wavy text

---
+++ app/components/Wave/Wave.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./WordAnimator.css";
+
+function WordAnimator({ word }) {
+  const words = word.split(" ");
+
+  return (
+    <div className="wave-container">
+      {words.map((word, index) => (
+        <React.Fragment key={index}>
+          <span className="wavy">
+            {word.split("").map((letter, index) => (
+              <span key={index} style={{ "--i": index + 1 }}>
+                {letter}
+              </span>
+            ))}
+          </span>
+          {index < words.length - 1 && <span>&nbsp;</span>}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
+
+export default WordAnimator;
