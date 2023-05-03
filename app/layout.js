dc74452b95a419318ccdd5655a9e55ff18ae84d4
queens kisivuli <queenskisivuli@gmail.com>
2023-05-03 12:36:38 +0300

    metadata discrtiption

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Nav from "./components/Nav/Nav";
 export const metadata = {
   title: "Queens Kisivuli-Experienced Software Engineer",
   description:
-    " Queens Kisivuli is an experienced software engineer specializing in full-stack development. With expertise in multiple programming languages and a track record of delivering high-quality, scalable solutions, Queens can help take your project to the next level. Visit His portfolio to see examples of her work and learn more about how she can help your business succeed",
+    "Queens Kisivuli is an experienced software engineer specializing in full-stack development. With expertise in multiple programming languages and a track record of delivering high-quality, scalable solutions, Queens can help take your project to the next level. Visit His portfolio to see examples of her work and learn more about how she can help your business succeed.",
 };
 
 export default function RootLayout({ children }) {
