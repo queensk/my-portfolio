69797b87322b27497f5e33a3879aff56d9688340
queens kisivuli <queenskisivuli@gmail.com>
2023-05-04 10:35:11 +0300

    code highlighte

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,46 +2,44 @@ import React from "react";
 import styles from "./page.module.css";
 import { SiStyledcomponents } from "react-icons/si";
 import { MdCastForEducation } from "react-icons/md";
-import CodeComponent from "../components/CodeComponent/CodeComponent";
+import CodeHighlighter from "../components/CodeComponent/CodeComponent";
+import Experience from "../components/Experience/Experience"
 
 const code = `
-\`\`\`javascript
-          class JanBaszczok {
+          class QueensKisivuli {
           // I'm ambitious and I love new challenges :)
           // My vast variety of skills is continuously expanding.
           constructor() {
-            this.name = 'Jan Baszczok';
-            this.dayOfBirthTimestamp = 602745592;
-            this.email = 'baszczok@yasio.dev';
+            this.name = 'Queens Kisivuli';
+            this.dayOfBirthTimestamp = 950734800;
+            this.email = 'queenskisivuli@gmail.com';
           }
           workExperience() {
             return [
-              { '2020-now': 'Full-stack Developer/Owner at Pixel Lab' },
-              { '2020-now': 'Full-stack Developer at Digitree Group S.A.' },
-              { '2017-2020': 'Front-end Developer at Digitree Group S.A.' },
-              { '2015-2017': 'Full-Stack Designer at Digitree Group S.A.' },
-              { '2013-2015': 'Graphic Designer at Digitree Group S.A.' },
-              { '2006-now': 'Full-Stack Designer/developer at Yasio.dev' },
+              { '2017-2020': 'Backend Developer AlX Software Engeneer' },
+              { '2022-2022': 'Full-stack Developer/NEA-GROUP-OF-GLOBAL-GIANTS' },
+              { '2022-2022': 'Network Management' },
             ];
           }
           education() {
             return [
-              { '2012-2014': 'Wyższa Szkoła Biznesu w Dąbrowie Górniczej - mgr IT' },
-              { '2008-2012': 'Wyższa Szkoła Technologii Informatycznych w Katowicach - inż. IT' },
+              { '2019-2023': 'BSC Computer Science Education Kirinyaga University' },
+              { '2022-2022': 'Full Stack Developer - Udacity'},
+              { '2022-2022': 'Cloud DevOps Engineer - Udacity'},
             ];
           }
           skills() {
             return [
-              'HTML/CSS/JS', 'Vue', 'Node.js', 'Redis/NATS/RabbitMQ', 'Bootstrap/Tailwind', 'Webpack/Gulp/Vite', 
+              'HTML/CSS/JS', 'Node.js', 'Redis/NATS/RabbitMQ', 'Bootstrap/Tailwind', 'Webpack/Gulp/Vite', 
               'SCSS/Less', 'npm/yarn/pnpm', 'Docker/k8s', 'PWA', 'SSR', 'SPA', 'GIT/CVS', 'Cordova', 'NativeScript',
               'Electron', 'Web-extensions', 'Web Sockets', 'Firebase, 'RWD/W3C/ARIA/WCAG', 'XSLT/Smarty/Twig', 'PHP',
               'MySQL/MongoDB/ORM', 'Wordpress', 'Photoshop', 'Illustrator', 'After Effects', 'Premiere', 'Motion design',
-              'UX/UI', 'DTP', 'C#', 'Unity', 'TypeScript', 'NestJS', 'Cypress', 'Jest', 'Nuxt', 'Quasar',
+              'DTP', 'C#', 'Unity', 'TypeScript', 'NestJS', 'Cypress', 'Jest', 'Nuxt', 'Quasar','Frontend', 'Backend',
+              'Full-Stack', 'Microservices', 'Distributed Systems', 'DevOps Engineer'
             ];
           }
         }
-\`\`\`
-`;
+        `;
 
 export default function About() {
   return (
@@ -50,113 +48,37 @@ export default function About() {
       <div className={styles.aboutContent}>
         <h1>About Me</h1>
         <div className={styles.aboutTemplate}>
-          <div className={styles.detailsContainer}>
-            <SiStyledcomponents />
-            <h3>Experience</h3>
-            <p>
-              2+ years <br />
-              Frontend Development
-            </p>
-          </div>
-          <div className={styles.detailsContainer}>
-            <SiStyledcomponents />
-            <h3>Experience</h3>
-            <p>
-              2+ years <br />
-              Frontend Development
-            </p>
-          </div>
-          <div className={styles.detailsContainer}>
-            <SiStyledcomponents />
-            <h3>Experience</h3>
-            <p>
-              2+ years <br />
-              Frontend Development
-            </p>
-          </div>
-          <div className={styles.detailsContainer}>
-            <MdCastForEducation />
-            <h3>Education</h3>
-            <p>
-              B.Sc. Bachelors Degree
-              <br />
-              M.Sc. Masters Degree
-            </p>
-          </div>
+          <Experience
+            className={styles.detailsContainer}
+            icon={<MdCastForEducation />}
+            Header="Experience"
+            experience="Frontend Development"
+            time={2}
+          />
+          <Experience
+            className={styles.detailsContainer}
+            icon={<SiStyledcomponents />}
+            Header="Experience"
+            experience="Backend Development"
+            time={2}
+          />
+          <Experience
+            className={styles.detailsContainer}
+            icon={<SiStyledcomponents />}
+            Header="Experience"
+            experience="DevOps Engineer"
+            time={1}
+          />
+          <Experience
+            className={styles.detailsContainer}
+            icon={<SiStyledcomponents />}
+            Header="Education"
+            experience="BSC Compute Science Degree"
+            time={4}
+          />
         </div>
         <div className={styles.textContainer}>
-          {/* <SyntaxHighlighter language="javascript" style={atomDark}>
-            {`class Jan Baszczok {
-    // I'm ambitious and I love new challenges :)
-    // My vast variety of skills is continuously expanding.
-    constructor() {
-      this.name = 'Jan Baszczok';
-      this.dayOfBirthTimestamp = 602745592;
-      this.email = 'baszczok@yasio.dev';
-    }
-    workExperience() {
-      return [
-        { '2020-now': 'Full-stack Developer/Owner at Pixel Lab' },
-        { '2020-now': 'Full-stack Developer at Digitree Group S.A.' },
-        { '2017-2020': 'Front-end Developer at Digitree Group S.A.' },
-        { '2015-2017': 'Full-Stack Designer at Digitree Group S.A.' },
-        { '2013-2015': 'Graphic Designer at Digitree Group S.A.' },
-        { '2006-now': 'Full-Stack Designer/developer at Yasio.dev' },
-      ];
-    }
-    education() {
-      return [
-        { '2012-2014': 'Wyższa Szkoła Biznesu w Dąbrowie Górniczej - mgr IT' },
-        { '2008-2012': 'Wyższa Szkoła Technologii Informatycznych w Katowicach - inż. IT' },
-      ];
-    }
-    skills() {
-      return [
-        'HTML/CSS/JS',
-        'Vue',
-        'Node.js',
-        'Redis/NATS/RabbitMQ',
-        'Bootstrap/Tailwind',
-        'Webpack/Gulp/Vite',
-        'SCSS/Less',
-        'npm/yarn/pnpm',
-        'Docker/k8s',
-        'PWA',
-        'SSR',
-        'SPA',
-        'GIT/CVS',
-        'Cordova',
-        'NativeScript',
-        'Electron',
-        'Web-extensions',
-        'Web Sockets',
-        'Firebase',
-        'RWD/W3C/ARIA/WCAG',
-        'XSLT/Smarty/Twig',
-        'PHP',
-        'MySQL/MongoDB/ORM',
-        'Wordpress',
-        'Photoshop',
-        'Illustrator',
-        'After Effects',
-        'Premiere',
-        'Motion design',
-        'UX/UI',
-        'DTP',
-        'C#',
-        'Unity',
-        'TypeScript',
-        'NestJS',
-        'Cypress',
-        'Jest',
-        'Nuxt',
-        'Quasar',
-      ];
-    }
-  }`}
-          </SyntaxHighlighter> */}
-
-          <CodeComponent code={code} />
+          <CodeHighlighter code={code} language="javascript" />
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis
             reprehenderit et laborum, rem, dolore eum quod voluptate
